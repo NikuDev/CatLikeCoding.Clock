48610655835e8de1e0a183d80fcb54f8e93d27c1
NiKuDev <niek_kuijken@hotmail.com>
2019-12-14 21:11:15 +0100

    - Added 'continuous' updating of the clock arms for more precision
    Signed-off-by: NiKuDev <niek_kuijken@hotmail.com>

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
     public Transform HoursTransform, MinutesTransform, SecondsTransform;
+    public bool Continuous;
+
     private readonly float _degreesPerHour = 30f;
     private readonly float _degreesPerMinute = 6f;
     private readonly float _degreesPerSecond = 6f;
@@ -16,7 +18,7 @@ public class Clock : MonoBehaviour
         Debug.Log(DateTime.Now);
 
         // use the function to set the arms to the current time
-        this.TransformArmsToCurrentTime();
+        this.TransformArmsToCurrentTimeDiscrete();
     }
 
     // Start is called before the first frame update
@@ -28,11 +30,18 @@ public class Clock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // use the function to set the arms to the current time
-        this.TransformArmsToCurrentTime();
+        // use the appropriate function to update the rotation of the arms
+        if (this.Continuous)
+        {
+            this.TransformArmsToCurrentTimeContinuous();
+        }
+        else
+        {
+            this.TransformArmsToCurrentTimeDiscrete();
+        }
     }
 
-    private void TransformArmsToCurrentTime()
+    private void TransformArmsToCurrentTimeDiscrete()
     {
         DateTime currentTime = DateTime.Now;
         this.HoursTransform.localRotation =
@@ -41,5 +50,15 @@ public class Clock : MonoBehaviour
             Quaternion.Euler(0, currentTime.Minute * _degreesPerMinute, 0);
         this.SecondsTransform.localRotation =
             Quaternion.Euler(0, currentTime.Second * _degreesPerSecond, 0);
+    }
+    private void TransformArmsToCurrentTimeContinuous()
+    {
+        TimeSpan currentTimePrecise = DateTime.Now.TimeOfDay;
+        this.HoursTransform.localRotation =
+            Quaternion.Euler(0, (float)currentTimePrecise.TotalHours * _degreesPerHour, 0);
+        this.MinutesTransform.localRotation =
+            Quaternion.Euler(0, (float)currentTimePrecise.TotalMinutes * _degreesPerMinute, 0);
+        this.SecondsTransform.localRotation =
+            Quaternion.Euler(0, (float)currentTimePrecise.TotalSeconds * _degreesPerSecond, 0);
     }
 }
